ad90d455eef75f703d4205265b18c48ed69d5475
slshkk <slshkk@icloud.com>
2017-07-01 12:57:38 +0300

    another one

--- 1.JavaSyntax/src/com/javarush/task/task09/task0902/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0902/Solution.java
@@ -10,26 +10,71 @@ public class Solution {
     }
 
     public static String method1() {
+
         method2();
+
         //напишите тут ваш код
+
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+
+        return stackTraceElements[2].getMethodName();
+
     }
+
+
 
     public static String method2() {
+
         method3();
+
         //напишите тут ваш код
+
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+
+        return stackTraceElements[2].getMethodName();
+
+
+
     }
+
+
 
     public static String method3() {
+
         method4();
+
         //напишите тут ваш код
+
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+
+        return stackTraceElements[2].getMethodName();
+
     }
+
+
 
     public static String method4() {
+
         method5();
+
         //напишите тут ваш код
+
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+
+        return stackTraceElements[2].getMethodName();
+
     }
 
+
+
     public static String method5() {
+
         //напишите тут ваш код
+
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+
+        return stackTraceElements[2].getMethodName();
+
     }
+
 }
---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0903/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task09.task0903;
+
+/* 
+Кто меня вызывал?
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        method1();
+    }
+
+    public static int method1() {
+        method2();
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        System.out.println((stackTraceElements[1].getMethodName()) + (" вызывается из ") + stackTraceElements[2].getLineNumber() + " строки");
+        return stackTraceElements[2].getLineNumber();
+
+    }
+
+    public static int method2() {
+        method3();
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        System.out.println((stackTraceElements[1].getMethodName()) + (" вызывается из ") + stackTraceElements[2].getLineNumber() + " строки");
+        return stackTraceElements[2].getLineNumber();
+    }
+
+    public static int method3() {
+        method4();
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        System.out.println((stackTraceElements[1].getMethodName()) + (" вызывается из ") + stackTraceElements[2].getLineNumber() + " строки");
+        return stackTraceElements[2].getLineNumber();
+    }
+
+    public static int method4() {
+        method5();
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        System.out.println((stackTraceElements[1].getMethodName()) + (" вызывается из ") + stackTraceElements[2].getLineNumber() + " строки");
+        return stackTraceElements[2].getLineNumber();
+    }
+
+    public static int method5() {
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        System.out.println((stackTraceElements[1].getMethodName()) + (" вызывается из ") + stackTraceElements[2].getLineNumber() + " строки");
+        return stackTraceElements[2].getLineNumber();
+    }
+}
